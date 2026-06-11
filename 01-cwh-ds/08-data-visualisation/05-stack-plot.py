0755bfbd0255bca9a89c33c09ef0e7ccf6218d2b
Shayan Ahmed Khan <shayanrizwank123@gmail.com>
2026-06-11 16:57:10 +0530

    d6 correctinon

--- 01-cwh-ds/08-data-visualisation/05-stack-plot.py
+++ 01-cwh-ds/08-data-visualisation/05-stack-plot.py
@@ -2,19 +2,19 @@
 import matplotlib.pyplot as plt
 
 days = [1,2,3,4,5,6,7]
-coding = [5,6,3,3,6,7,8]
-sleeping = [8,8,9,10,9,10,11]
-netflix = [4,2,3,4,1,2,2]
-content_creation = [0,0,0,2,0,0,0.5]
+coding = [7,6,4,5,6,7,4]
+exploratory_data_analysis = [6,6,8,7,9,7,11]
+netflix = [1,2,1,4,1,2,3]
+content_creation = [0,0,0,1,0,0,0.5]
 
-labels = ["Coding", "Sleeping", "Netflix", "Content creation"]
+labels = ["Coding", "coding", "exploratory_data_analysis", "Content creation"]
 colors = ['skyblue', 'lightgreen', 'gold', 'lightcoral']
 
 plt.figure(figsize=(10,6))    # charts scale size in inches
 plt.stackplot(
     days, 
     coding, 
-    sleeping, 
+    exploratory_data_analysis, 
     netflix, 
     content_creation, 
     labels=labels, 
