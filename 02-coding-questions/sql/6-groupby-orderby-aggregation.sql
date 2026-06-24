5deda41d560c0ef2dbd0f2f0835a342389ac1a9d
Shayan Ahmed Khan <shayanrizwank123@gmail.com>
2026-06-24 13:48:22 +0530

    added google colab notes

--- 02-coding-questions/sql/6-groupby-orderby-aggregation.sql
+++
@@ -1,8 +0,0 @@
--- https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true
-
-
-SELECT (months * salary) AS earnings, COUNT(*)
-FROM Employee
-GROUP BY earnings
-ORDER BY earnings DESC
-LIMIT 1;
